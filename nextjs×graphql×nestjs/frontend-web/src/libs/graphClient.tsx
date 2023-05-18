09503eef5b7ab05b7f67e3249108e31d020e6c3b
hidaken991018 <ed.hidaken@gmail.com>
2023-05-18 23:40:56 +0900

    レスポンスヘッダ―に権限を追加するよう修正

--- nextjs×graphql×nestjs/frontend-web/src/libs/graphClient.tsx
+++ nextjs×graphql×nestjs/frontend-web/src/libs/graphClient.tsx
@@ -1,6 +1,29 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-const client = new ApolloClient({
+import { COOKIE_KEY } from '@/constants/auth';
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import Cookies from 'js-cookie';
+
+// リクエストヘッダー設定
+const middlewareLink = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: Cookies.get(COOKIE_KEY.AUTHORIZATION) || null,
+    },
+  }));
+
+  return forward(operation);
+});
+
+const httpLink = new HttpLink({
   uri: process.env.NEXT_PUBLIC_LOCAL_IF_HOST,
+});
+const client = new ApolloClient({
+  link: ApolloLink.from([middlewareLink, httpLink]),
   cache: new InMemoryCache(),
 });
 export default client;
